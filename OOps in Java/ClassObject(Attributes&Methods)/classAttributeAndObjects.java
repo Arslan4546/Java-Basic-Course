b2d1de3cda41a61b0ae6f6d8a74fcc64049df0b1
Arslan Tariq <arslantariq4546@gmail.com>
2024-08-16 13:20:39 +0500

    add some files

---
+++ OOps in Java/ClassObject(Attributes&Methods)/classAttributeAndObjects.java
@@ -0,0 +1,35 @@
+
+//class and attributes
+
+public class Main {
+    int x = 5;
+    int y = 3;
+  }
+
+   //class and creating class object and access the class attribute
+
+
+  public class Main {
+    int x = 5;
+     public static void main(String[] args) {
+      Main myObj = new Main();
+      System.out.println(myObj.x);
+    }
+  }
+
+
+
+
+  //class and creating object of that class and change the attribute value
+
+  
+  public class Main {
+    int x;
+  
+    public static void main(String[] args) {
+      Main myObj = new Main();
+      myObj.x = 40;
+      System.out.println(myObj.x);
+    }
+  }
+
