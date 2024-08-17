8f29c66d60851387184f77de80db38fdeca119b0
Arslan Tariq <arslantariq4546@gmail.com>
2024-08-17 22:21:05 +0500

    add java references

---
+++ OOps in Java/MainConceptsInOOps/java References/final keyword/final.java
@@ -0,0 +1,21 @@
+
+// Set a variable to final, to prevent it from being overridden/modified:
+
+// public class Main {
+//   final int x = 10;
+
+//   public static void main(String[] args) {
+//     Main myObj = new Main();
+//     myObj.x = 25; // will generate an error: cannot assign a value to a final variable
+//     System.out.println(myObj.x);
+//   }
+// }
+
+
+
+// Definition and Usage
+// The final keyword is a non-access modifier used for classes, attributes and methods, which makes them non-changeable (impossible to inherit or override).
+
+// The final keyword is useful when you want a variable to always store the same value, like PI (3.14159...).
+
+// The final keyword is called a "modifier". You will learn more about these in the Java Modifiers Chapter.
