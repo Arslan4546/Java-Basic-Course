e39c7a7aae7f94ed163e898a721df19843820a36
Arslan Tariq <arslantariq4546@gmail.com>
2024-08-17 08:13:34 +0500

    add packages files

---
+++ OOps in Java/Packages/demopackage/hannan.java
@@ -0,0 +1,9 @@
+
+package  demopackage;
+
+
+public class hannan {
+    public static void main(String[] args) {
+        System.out.println("This is Hannan Program");
+    }
+}
---
+++ OOps in Java/Packages/demopackage/mannan.java
@@ -0,0 +1,9 @@
+
+package  demopackage;
+
+
+public class mannan {
+    public static void main(String[] args) {
+        System.out.println("This is Mannan Program");
+    }
+}
