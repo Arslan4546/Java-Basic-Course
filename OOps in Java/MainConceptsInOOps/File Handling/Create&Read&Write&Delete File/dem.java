558ddbdb74022baae312fbc33b17995b0ac61735
Arslan Tariq <arslantariq4546@gmail.com>
2024-08-17 23:53:31 +0500

    add file handling concept files

---
+++ OOps in Java/MainConceptsInOOps/File Handling/Create&Read&Write&Delete File/dem.java
@@ -0,0 +1,58 @@
+// import java.io.File;
+// import java.io.IOException;
+
+// public class dem {
+    
+//     public static void main(String[] args) throws IOException {
+       
+
+
+        // File myFile = new  File("demo.txt");
+        // try {
+        //     myFile.createNewFile(); 
+        // } catch (IOException  e) {
+        //     System.out.println("unable to create file.");
+        // }
+
+
+            // try {
+            //     FileWriter fileWriter = new FileWriter("demo.txt");
+            //     fileWriter.write("This is the demo line.\n  ok now by");
+            //     fileWriter.close();
+
+            // } catch (IOException e) {
+            //   e.printStackTrace();
+            // }
+         
+            // File myFile = new  File("my.txt");
+            // //File myFile = new  File("my.txt");
+            // try {
+            //     Scanner sc = new Scanner(myFile);
+            //     while (sc.hasNextLine()) {
+
+            //         String line = sc.nextLine();
+            //         System.out.println(line); 
+                    
+            //     }
+            //     sc.close();
+                
+            // } catch (IOException e) {
+
+            //     e.printStackTrace();
+            // }
+
+
+            // File myFile = new  File("my.txt");
+            // if (myFile.delete()) {
+
+            //     System.out.println("YOu have deleted this file"  + myFile.getName() );
+                
+            // }
+            // else{
+            //     System.out.println("Error while deleting this file.");
+            // }
+
+   
+// }
+// }
+
