ce1f35017854bcc50e41736562e0d05b3ebb5793
Arslan Tariq <arslantariq4546@gmail.com>
2024-08-19 00:25:14 +0500

    adding variables types concepts

--- OOps in Java/MainConceptsInOOps/Constructor/ConstructorInJava.java
+++ OOps in Java/MainConceptsInOOps/Constructor/ConstructorInJava.java
@@ -4,19 +4,19 @@
 
 //create constructor of a class and declare & initialize a variable into that and access it by creating object of that class and print it 
 // Create a Main class
-public class Main {
-    int x;  // Create a class attribute
+// public class Main {
+//     int x;  // Create a class attribute
   
-    // Create a class constructor for the Main class
-    public Main() {
-      x = 5;  // Set the initial value for the class attribute x
-    }
+//     // Create a class constructor for the Main class
+//     public Main() {
+//       x = 5;  // Set the initial value for the class attribute x
+//     }
   
-    public static void main(String[] args) {
-      Main myObj = new Main(); // Create an object of class Main (This will call the constructor)
-      System.out.println(myObj.x); // Print the value of x
-    }
-  }
+//     public static void main(String[] args) {
+//       Main myObj = new Main(); // Create an object of class Main (This will call the constructor)
+//       System.out.println(myObj.x); // Print the value of x
+//     }
+//   }
   
 
 
